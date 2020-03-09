c53809cb2370009f4f9fd8e57a89422515feb7e6
aasehic <aasehic@miners.utep.edu>
2020-03-09 14:50:45 -0600

    humbler beginnings

--- src/AminoAcidResourcesTester.java
+++ src/AminoAcidResourcesTester.java
@@ -1,7 +1,6 @@
-import static org.junit.jupiter.api.Assertions.*;
-import java.util.concurrent.TimeUnit;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class AminoAcidResourcesTester{
 
